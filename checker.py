4a5b98bbc36eb350f54228bbe354a1666fa8366f
Shawn Pavey <shawn.pavey@yahoo.com>
2025-01-30 12:45:54 -0600

    Got rid of extra
    Got rid of extra

--- checker.py
+++
@@ -1,8 +0,0 @@
-import toml
-
-try:
-    with open("pyproject.toml", "r") as f:
-        toml.load(f)
-    print("TOML is valid!")
-except toml.TomlDecodeError as e:
-    print(f"TOML file is invalid: {e}")
