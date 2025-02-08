8c738e5be0454e50cc999b9d0c446b647901135d
Shawn Pavey <shawn.pavey@yahoo.com>
2025-02-08 12:12:42 -0600

    Added excel initialization either by args (automatically detect path and shet name) or by kwargs (excel_path, sheet_name). Also fixed some unexpected behavior when zlab is not given for a dataframe (added function check_labels_in_DF which is called right before ever plot and returns temporary labels as None if they do not correspond to a dataframe column name).

--- readyplot/boxwhisker_plotter.py
+++ readyplot/boxwhisker_plotter.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 27 14:34:59 2025
-Custom plotter function which copies styles used by Shawn Pavey in Prism. Many
+Custom plotter function which copies styles used by Shawn Pavey in plotting software. Many
 inputs are customizable, but defaults work well. This script contains two
-functions: custom_plotter (full plotting + formating) and prism_reskin (only
+functions: custom_plotter (full plotting + formating) and plotting software (only
 reformats given figures).
 @author: paveyboys
 """
